9030a0196956bf577270a9f4522616064505d748
Rupesh Saini <rupeshsaini32110@gmail.com>
2025-02-08 17:12:14 +0530

    Add files via upload

---
+++ Assignment 1/Q3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cmath>
+#include<string>
+#include<iomanip>
+using namespace std;
+int main(){
+    string str;
+    cout<<"Enter a word = ";
+    getline(cin,str);
+    int len = str.size();
+    bool pal = true;
+    int i;
+    for (i=0;i<len/2;i++){
+        if ((str[i])!=(str[len-i-1])){
+            pal=false;
+            break;
+        }
+    }
+    pal?cout<<"word is a palindrome":cout<<"Word is not a palindrome";
+    for(int i=0; i<str.length(); i++){
+        if(str[i]=='a'||str[i]=='e'||str[i]=='i'||str[i]=='o'||str[i]=='u'){
+            str[i]='$';
+        }
+    }
+    cout<<endl<<"String after replacing characters by $ = "<<str;
+    return 0;
+}
